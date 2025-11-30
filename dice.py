c3d2f79264a633f7649049b7fb51f5df792d9d23
Onkar Shintre <shintreomkar07@gmail.com>
2025-11-30 10:55:41 +0530

    Update dice.py

--- dice.py
+++ dice.py
@@ -3,10 +3,11 @@ import random
 again = True
 
 while again :
-    print(random.randint(1, 6))
+    print(random.randint(1, 6))  #Number
     another_roll = input("Want to roll the dice again? (yes/no):")
     if another_roll.lower() == "yes":
         continue
     else:
         break
+
     
